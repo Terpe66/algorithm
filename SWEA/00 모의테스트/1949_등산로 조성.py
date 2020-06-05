02e9787a4bd83a0899b1f00882e1af60d9365430
Lee Wonjin <terpe66@gmail.com>
2020-06-05 22:18:22 +0900

    등산로 조성

--- SWEA/00 모의테스트/1949_등산로 조성.py
+++ SWEA/00 모의테스트/1949_등산로 조성.py
@@ -1,52 +1,36 @@
 import sys
 sys.stdin = open("1949.txt")
 
-dr = [1, -1, 0, 0]
-dc = [0, 0, -1, 1]
 
+drc = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 for T in range(int(input())):
     answer = 0
-    length, deep = map(int, input().split())
-    MAP = []
-    highest = []
-    maxNum = 0
-    for i in range(length):
-        inputs = list(map(int, input().split()))
-        for j in range(length):
-            if inputs[j] > maxNum:
-                maxNum = inputs[j]
-        MAP.append(inputs)
+    N, K = map(int, input().split())
+    mountain = [list(map(int, input().split())) for _ in range(N)]
+    start = []
+    m = 0
+    for r in range(N):
+        for c in range(N):
+            if mountain[r][c] > m:
+                start = [(r, c)]
+                m = mountain[r][c]
+            elif mountain[r][c] == m:
+                start.append((r, c))
 
-    for i in range(length):
-        for j in range(length):
-            if MAP[i][j] == maxNum:
-                highest.append((i, j, 1, maxNum, deep, [(i, j)]))
+    for row, col in start:
+        temp = [(row, col, K, mountain[row][col], [(row, col)])]
+        while temp:
+            r, c, k, now, past = temp.pop()
 
-    while highest:
-        row, col, cnt, num, d, pasts = highest.pop()
+            answer = max(answer, len(past))
 
-        if answer < cnt:
-            answer = cnt
+            for y, x in drc:
+                nr, nc = r + y, c + x
+                if 0 <= nr < N and 0 <= nc < N and (nr, nc) not in past:
+                    new = mountain[nr][nc]
+                    if now > new:
+                        temp.append((nr, nc, k, new, past + [(nr, nc)]))
+                    elif k and new - k < now <= new:
+                        temp.append((nr, nc, 0, new - (new - now) - 1, past + [(nr, nc)]))
 
-        for i in range(4):
-            nrow, ncol = row + dr[i], col + dc[i]
-            if 0 <= nrow < length and 0 <= ncol < length and (nrow, ncol) not in pasts:
-                new = MAP[nrow][ncol]
-                if num > new:
-                    highest.append((nrow, ncol, cnt + 1, new, d, pasts + [(nrow, ncol)]))
-                    continue
-                if d != deep:
-                    continue
-                if num == new:
-                    highest.append((nrow, ncol, cnt + 1, new - 1, d - 1, pasts + [(nrow, ncol)]))
-                    continue
-                if num > new - d:
-                    nd = 1
-                    while nd <= d:
-                        if num > new - nd:
-                            break
-                        nd += 1
-                    if num > new - nd:
-                        highest.append((nrow, ncol, cnt + 1, new - nd, d - nd, pasts + [(nrow, ncol)]))
-
-    print("#{} {}".format(T+1, answer))
+    print("#{} {}".format(T + 1, answer))
